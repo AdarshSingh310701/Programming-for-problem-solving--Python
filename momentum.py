0473f72482efdb0b55570cbf03db15d217b4b2c0
Adarsh Singh <adarsh31072001singh@gmail.com>
2023-04-04 23:34:32 +0530

    Add files via upload

---
+++ momentum.py
@@ -0,0 +1,5 @@
+print("Name- Adarsh Singh, Roll no.-17, PPS Practical 1")
+m=int(input("Enter mass of object in kilograms: "))
+c=int(input("Enter velocity of object in meters per second: "))
+e=m*c**2
+print("Momentum of the object is calculated as: ",e,"kg(m/s)^2")
